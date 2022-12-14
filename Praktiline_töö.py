350bf3f9543ef2d5f9e76d691ab7463f99329ebd
opilane <opilane@HP-CZC6297BB7>
2022-12-14 11:34:37 +0200

    Tingimuslikoperator

--- Praktiline_töö.py
+++ Praktiline_töö.py
@@ -1,27 +1,65 @@
-from math import * # * - kõik funktsioonid moodulist
+from ast import If
+from math import *
+from operator import ifloordiv # * - kõik funktsioonid moodulist
 from random import *
 from stringprep import c22_specials
 #import math          math.funktsioonid()
+#14/12/22
+#Ülesanne "Ema robot".
+print("Ema: mis hinde sa koolis said? ")
+a=input("Sisesta: ")
+print(a.isalpha(), a.isdigit())
+if a.isdigit() and int(a)>0 and int(a)<=5:
+    a=int(a)
+    if a==5:
+        pass
+    elif a==4:
+        pass
+    elif a==3:
+        pass
+    elif a==2 or a==1:
+        pass
+else:
+    print("Sa valesti vasta!")
+
+
+
+
+
 #12/12/22
 #1
 print("Puu läbimõõdu arvutamine")
 #Kirjuta programm, mis küsib kasutaja käest puu ümbermõõdu ning teatab selle peae puu läbimõõdu.
-C=float(input("Anna ümbermõõd: "))
-d=round(C/pi,2)
+try:
+    C=float(input("Anna ümbermõõd: "))
+    if C<0:
+    
+        d=round(C/pi,2)
+        print(f"Puu läbimõõt = {d}")
+    else:
+        print("C peab olema suurem kui 0")
+except:
+    print("Andmetüüp on vale")
 
-print(f"Puu läbimõõt = {d}")
 #2
 print("Ristkülikukujulise maatüki diagonaal pikk")
-N=float(input("Sisesta N: "))
-M=float(input("Sisesta M: "))
-d=round(sqrt(N**2+M**2),2)
-print("Ristkülikukujulise maatüki diagonaal pikk: ", round(d,2))
+try:
+    N=float(input("Sisesta N: "))
+    M=float(input("Sisesta M: "))
+    if M<0 and N<0:
+        d=round(sqrt(N**2+M**2),2)
+        print("Ristkülikukujulise maatüki diagonaal pikk: ", round(d,2))
+except:
+    print("M ja N vaja sisestada float formaadis")
 
 #3 
-aeg = float(input("Mitu tundi kulus sõiduks? "))
-teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
-kiirus = teepikkus / aeg
-
+try:
+    aeg = float(input("Mitu tundi kulus sõiduks? "))
+    teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
+    if aeg>0 and teepikkus>0:
+        kiirus = teepikkus / aeg
+except:
+    print("aeg ja teepikkus vaja sisestada float formaadis")
 print("Sinu kiirus oli " + str(kiirus) + " km/h")
 
 #4
@@ -30,7 +68,10 @@ A2=int(input("Teine arv: "))
 A3=int(input("Kolmas arv: "))
 A4=int(input("Neljas arv: "))
 A5=int(input("Viies arv: "))
-K=(A1+A2+A3+A4+A5)/5
+if A1>0 and A2>0 and A3>0 and A4>0 and A5>0:
+    K=(A1+A2+A3+A4+A5)/5
+else: 
+    print("aeg ja teepikkus vaja sisestada float formaadis")
 print(f"Keskmine on {K}")
 
 #5
@@ -72,3 +113,4 @@ M=int(input("Sisetajad aja minutid: ")) #1h=60min
 H=M//60 #h
 M=M%60 #min
 print(f"{H}:{M}")
+
